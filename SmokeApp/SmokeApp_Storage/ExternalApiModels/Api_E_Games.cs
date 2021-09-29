0329afeb2d307be952d082183fe0ea6edf9ac997
Cory <cory.tinsley@revature.net>
2021-09-29 01:35:30 -0400

    adding external api calls and models. ready to connect to front end for testing

--- SmokeApp/SmokeApp_Storage/ExternalApiModels/Api_E_Games.cs
+++ SmokeApp/SmokeApp_Storage/ExternalApiModels/Api_E_Games.cs
@@ -3,20 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SmokeApp_Storage.Repositories;
 
 namespace SmokeApp_Storage.ExternalApiModels
 {
-  public class Api_E_Games
+  public class Api_E_Games : Result
   {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public string Released { get; set; }
-    public string BackgroundImage { get; set; }
-    public string Rating { get; set; }
-    public Object[] Platforms { get; set; }
+    public int? Id { get; }
+    public string Name { get; }
+    public string Description { get; }
+    public string Released { get; }
+    public string BackgroundImage { get; }
+    public string Rating { get; }
+    public Object Platforms { get; }
 
-    static async Task SearchGames()
+        private int count;
+
+    public Api_E_Games(int? id = null, string name = null, string description = null, string released = null, string backgroundImage = null, string rating = null, dynamic[] platforms = null)
+    {
+      Id = id;
+      Name = name;
+      Description = description;
+      Released = released;
+      BackgroundImage = backgroundImage;
+      Rating = rating;
+      Platforms = platforms;
+            client = RawgRepository.Instance;
+
+    }
+
+    public async Task<Api_E_Game[]> GetGamesAsync()
+    {
+      Api_E_Game games = await client.SendRequestAsync<Api_E_Game>(RawgRepository.Endpoint + $"games/1?key={client.apiKey}");
+      return games.Initialize();
+    }
+
+        //internal Api_E_Game[] Initialize()
+        //{
+        //    if (count <= 0)
+        //        return null;
+
+        //    var dlcs = new Api_E_Game[count];
+        //    game[0] = this;
+        //    for (int i = 1; i < count; i++)
+        //        game[i] = new Api_E_Game(count, i, data);
+        //    return dlcs;
+        //}
+
+        static async Task SearchGames()
     {
 
     }
--- SmokeApp/SmokeApp_Storage/Repositories/RawgRepository.cs
+++ SmokeApp/SmokeApp_Storage/Repositories/RawgRepository.cs
@@ -1,18 +1,17 @@
-using SmokeApp_Storage.ExternalApiModels;
+using Newtonsoft.Json;
 using System;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace SmokeApp_Storage.Repositories
 {
   public class RawgRepository
   {
+    public string apiKey { get; set; }
     private static RawgRepository _rawgRepository;
     private readonly HttpClient client;
     internal const string Endpoint = "https://api.rawg.io/api/";
-    private static readonly string apiKey = "529258b2f69a47c79b806e2c88a9c75f";
 
 
     public static RawgRepository Instance
@@ -30,27 +29,60 @@ namespace SmokeApp_Storage.Repositories
 
     public RawgRepository()
     {
+      apiKey = "529258b2f69a47c79b806e2c88a9c75f";
+
       client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip });
+      client.DefaultRequestHeaders.Add("Api_Key", apiKey);
+            client.Timeout = TimeSpan.FromMinutes(10);
     }
 
-    public static async Task searchGamesAsync(string path)
+    // public static async Task searchGamesAsync(string path)
+    // {
+    //   Api_E_Games games = null;
+    //   HttpRequestMessage request = new HttpRequestMessage();
+    //   request.RequestUri = uri;
+    //   request.Method = HttpMethod.Get;
+    //   request.Headers.Add("api_Key", apiKey);
+    //   HttpResponseMessage response = await client.GetAsync(request);
+
+
+    // }
+
+
+    internal async Task<T> SendRequestAsync<T>(string query) where T : Result
     {
-      Api_E_Games games = null;
-      HttpRequestMessage request = new HttpRequestMessage();
-      request.RequestUri = uri;
-      request.Method = HttpMethod.Get;
-      request.Headers.Add("api_Key", apiKey);
-      HttpResponseMessage response = await client.GetAsync(request);
+      HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, query);
+            Console.WriteLine(request.ToString());
+            Console.WriteLine(request.RequestUri.AbsoluteUri);
 
+            HttpResponseMessage response = await client.SendAsync(request);
+            //HttpResponseMessage response = await client.GetAsync(request.RequestUri.AbsoluteUri);
+            //HttpResponseMessage response = await client.GetStreamAsync(request.RequestUri);
 
+            Console.WriteLine("something");
+      if (response.StatusCode == HttpStatusCode.NotFound)
+        return new Result().Initialize(response.StatusCode, this) as T;
+
+      string content = await response.Content.ReadAsStringAsync();
+      var data = JsonConvert.DeserializeObject<dynamic>(content);
+
+      try
+      {
+        bool redirect = data.redirect;
+        return await SendRequestAsync<T>(Endpoint + $"games/{data.slug}");
+      }
+      catch
+      {
+        return JsonConvert.DeserializeObject<T>(content).Initialize(HttpStatusCode.OK, this) as T;
+      }
     }
 
-    static async Task RunAsync()
-    {
-      client.BaseAddress = new Uri("http://localhost/4200");
-      client.DefaultRequestHeaders.Accept.Clear();
-      client.DefaultRequestHeaders.Accept.Add(
-          new MediaTypeWithQualityHeaderValue("application/json"));
-    }
+    // static async Task RunAsync()
+    // {
+    //   client.BaseAddress = new Uri("http://localhost/4200");
+    //   client.DefaultRequestHeaders.Accept.Clear();
+    //   client.DefaultRequestHeaders.Accept.Add(
+    //       new MediaTypeWithQualityHeaderValue("application/json"));
+    // }
   }
 }
